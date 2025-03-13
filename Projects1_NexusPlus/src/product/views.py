3a986e97da33dcc6540be6a45c6f4bf3b7920ebb
Luga <yunusovaugust@gmail.com>
2025-03-13 15:35:19 +0500

    :|

--- Projects1_NexusPlus/src/product/views.py
+++ Projects1_NexusPlus/src/product/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from . import models
+from django.db.models import Prefetch
 
 def products(request):
-    return render(request, 'product.html', {})
+    products = models.Product.objects.prefetch_related(
+        Prefetch('images', queryset=models.ProductImage.objects.filter(is_main=True), to_attr='main_image'))
+    ctx = {
+        'products': products
+    }
+    return render(request, 'product.html', ctx)
