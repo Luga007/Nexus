099137c03116aaffb8cbbada306da15cf27dbb34
Luga <yunusovaugust@gmail.com>
2025-04-13 22:15:08 +0500

    done

--- Projects1_NexusPlus/src/blog/views.py
+++ Projects1_NexusPlus/src/blog/views.py
@@ -4,17 +4,23 @@ from django.db.models import Count
 from . import models
 from django.db.models import Prefetch
 from .models import Blog
-# Create your views here.
+from django.core.paginator import Paginator
+
 def blog(request):
+    page = request.GET.get('page', 1)
     category = Category.objects.annotate(counts=Count('product'))
     blogs = models.Blog.objects.prefetch_related(
         Prefetch('images', queryset=models.BlogImage.objects.all(), to_attr='main_image'),
     )
     category_blog = Blog.objects.annotate(counts=Count('id'))
+    paginator = Paginator(blogs, 2)
+    page_obj = paginator.get_page(page)
     ctx = {
         'categories': category,
         'blogs': blogs,
         'category_blog': category_blog,
+        'page_obj': page_obj,
+        'countOfBlogs': page,
     }
     return render(request, 'blog.html', ctx)
 
